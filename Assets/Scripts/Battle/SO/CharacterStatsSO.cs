b1afa83b054e0023d25c3310b39ee562a2319c07
Zeatlan <salimalexo@gmail.com>
2023-06-30 17:29:25 +0200

    feat: add menu header into CharacterStatsSO Scriptable object

--- Assets/Scripts/Battle/SO/CharacterStatsSO.cs
+++ Assets/Scripts/Battle/SO/CharacterStatsSO.cs
@@ -8,26 +8,31 @@ namespace BattleSystem.SO
     public class CharacterStatsSO : ScriptableObject
     {
         // Informations
+        [Header("Infos générale")]
         public string characterName;
         public Sprite icon;
         public AbilitySO skill;
 
         // Stats d'attaque
+        [Header("Statistiques d'attaque")]
         public int baseHealth;
         public int baseMana;
         public int basePhysicalDamage;
         public int baseMagicalDamage;
 
         // Portées
+        [Header("Portées")]
         public int baseRange = 3;
         public int baseAtkRange = 1;
 
         // Pourcentages
+        [Header("Pourcentages")]
         public float baseAccuraccyRate = 0.95f;
         public float baseDodgeRate = 0.1f; // Esquive et de double coup
         public float baseCriticalRate = 0.1f;
 
         // Stats
+        [Header("Statistiques générales")]
         public int baseStrength; // Influe sur les dégâts physique
         public int baseMagic; // Influe sur les dégâts magiques / Mana
         public int baseDexterity; // Influe sur les points de vue
